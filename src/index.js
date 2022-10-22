ec232bd3fc8c54c3ccb47417c00c70d6386bc8af
ncumplido <ncumplido@msn.com>
2022-10-22 13:46:16 +0100

    Do everything up to lesson 24: JSX-Javascript, this commit includes a lot of tidy up from previous courses that involve inline css, components

--- src/index.js
+++ src/index.js
@@ -1,17 +1,50 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function BookList(){
+  return <section className="bookList"> 
+    <Book />
+  </section>;
+  
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const Book = () => {
+  return (
+   <article className="book">
+      <h1>The Walking Dead</h1>
+      <h4>Robert Kirkman</h4>
+      <img src="https://upload.wikimedia.org/wikipedia/en/c/cf/WalkingDead1.jpg" alt="Grocery Basket" />
+    </article>
+  )
+}
+
+ReactDOM.render(<BookList />, document.getElementById('root'));
+
+//ReactDOM.render(<h2>Hello</h2>, document.getElementById('root'));
+//ReactDOM.render(<index/>, document.getElementById('root'));
+
+
+//Use "rafce" to quickly insert code, look at the extension for more
+// const Greeting = () => {
+//   return React.createElement(
+//     'div', 
+//     {}, 
+//     React.createElement('h1', {}, 'hello world')
+//   );
+// };
+// function BookList(){
+//   return (
+//   <div>
+//     <Hello />
+//     <Message />
+//   </div>
+//   )
+// }
+
+// const Hello = () => <h2>Hey hey hey</h2>;
+
+// //Explicit return:
+// const Message = () => {
+//   return <p>Yo yo yo</p>;
+// }
